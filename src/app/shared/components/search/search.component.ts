bd2e0ae52f5f6509463441d599bfc6c2d7c212ff
Lika Ghunashvili <l_ghunashvili@cu.edu.ge>
2022-02-01 18:24:16 +0400

    added search functionality

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { debounceTime, distinctUntilChanged, fromEvent, map, of } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, distinctUntilChanged, filter, fromEvent, map, of, switchMap, tap } from 'rxjs';
 import { MoviesService } from 'src/app/core/services/movies.service';
 import { SearchService } from 'src/app/core/services/search.service';
 @Component({
@@ -7,27 +7,44 @@ import { SearchService } from 'src/app/core/services/search.service';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, AfterViewInit {
   searchedMovieName!: string;
   genresArr: any = [];
   searchedMoviesArr: any = [];
   constructor(private search: SearchService, private movie: MoviesService) {}
-  @ViewChild('nameInput') searchTextRef: any;
+
+  // searchBox = document.getElementById('search-box') as HTMLInputElement;
+  @ViewChild('nameInput', { static: true }) name!: ElementRef;
+  typeahead:any;
   ngOnInit(): void {
     this.movie
       .getGenres()
       .subscribe((data: any) => (this.genresArr = data.genres));
     // console.log(this.searchedMoviesArr);
+
+
+  }
+  ngAfterViewInit() {
+    console.log(1);
+    
+   this.typeahead = fromEvent(this.name.nativeElement, 'keyup').pipe(
+      map((e:any) => (e.target as HTMLInputElement).value),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(searchTerm =>  { 
+        // console.log(searchTerm);
+        
+       return this.search.getSearchedMovie(searchTerm)
+      }),
+      tap((data)=> {console.log(data); return data}
+      ))
+
+    
   }
   searchMovie() {
-    fromEvent(this.searchTextRef.nativeElement, 'keyup')
-    .pipe(
-        debounceTime(500),
-        distinctUntilChanged()
-    ).subscribe(
-      value => console.log(value)
-    )
-   
+   this.typeahead.subscribe((data:any) => {
+    console.log(data);
+  });
     // this.search.getSearchedMovie(this.searchedMovieName)
     // .pipe(
     //   distinctUntilChanged(),
