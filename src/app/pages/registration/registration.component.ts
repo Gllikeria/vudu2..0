389d2f2a83830033d0ddf72663e6a466d31008f2
Lika Ghunashvili <l_ghunashvili@cu.edu.ge>
2022-02-16 12:00:06 +0400

    added languages

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -8,15 +8,32 @@ import { passwordValidator } from './form-validators';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-  today: any;
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
+  languages: string[] = ['English', 'Georgian', 'German', 'Spanish', 'Russian'];
+  contactForm: any;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {
-    // this.today =new Date();
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group(
+      {
+        firstname: ['', [Validators.required]],
+        email: [
+          '',
+          [Validators.required, Validators.pattern(this.emailPattern)],
+        ],
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+        phoneNumber: ['', [Validators.required]],
+        dateOfBirth: ['', Validators.required],
+        gender: ['', [Validators.required]],
+        language: ['', [Validators.required]],
+        zip: ['', [Validators.required]],
+        about: ['', [Validators.required]],
+      },
+      { validator: passwordValidator }
+    );
   }
-
-  contactForm:any;
-  openTab: any;
-  private emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
 
   homeComponent(e: any) {
     this.router.navigate(['home']);
@@ -51,23 +68,8 @@ export class RegistrationComponent implements OnInit {
   get about() {
     return this.contactForm.get('about');
   }
-  languages:string[] = ['English', 'Georgian', 'German'];
-  ngOnInit(): void {
-    this.contactForm = this.formBuilder.group({
-      firstname: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
-      password: ['', [Validators.required,]],
-      confirmPassword: ['', [Validators.required,]],
-      phoneNumber: ['', [Validators.required,]],
-      dateOfBirth: ['',Validators.required],
-      gender: ['', [Validators.required,]],
-      language: ['', [Validators.required,]],
-      zip: ['', [Validators.required,]],
-      about: ['', [Validators.required,]],
-    }, { validator: passwordValidator })
-  }
-  onSubmit(){
+  onSubmit() {
     console.log(this.contactForm.value);
-    this.contactForm.reset()
+    this.contactForm.reset();
   }
 }
